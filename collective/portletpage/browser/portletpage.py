a650729b1b47158268a78d627bc960b3a5e23226
Davide Targa <davide.targa@gmail.com>
2012-06-21 08:47:58 +0200

    Added methods for view rendering.

--- collective/portletpage/browser/portletpage.py
+++ collective/portletpage/browser/portletpage.py
@@ -16,9 +16,13 @@ class Base(BrowserView):
 
 class FourColumns(Base):
     """A four-column layout.
+
        |ColumnFirst|ColumnSecond|ColumnThird|ColumnFourth|
+       ---------------------------------------------------
        |                  ColumnTop                      |
+       ---------------------------------------------------
        |    ColumnMiddleFirst   |   ColumnMiddleSecond   |
+       ---------------------------------------------------
        |                  ColumnBottom                   |
     """
 
@@ -40,6 +44,27 @@ class FourColumns(Base):
         ploneview = self.plone_view()
         return ploneview.have_portlets('collective.portletpage.fourthcolumn', view=self)
 
+    def fourColumnCount(self):
+        """Returns the number of columns for the four columns section
+        """
+        i = 0
+        if self.hasColumnFirst():
+            i = i + 1
+        if self.hasColumnSecond():
+            i = i + 1
+        if self.hasColumnThird():
+            i = i + 1
+        if self.hasColumnFourth():
+            i = i + 1
+        return i
+
+    def columnCountInWords(self, number):
+        """Returns a number (in words).
+           Accepts a number as parameter.
+        """
+        numbers = {1:'one', 2:'two', 3:'three', 4:'four'}
+        return numbers[number]
+
     def hasColumnTop(self):
         ploneview = self.plone_view()
         return ploneview.have_portlets('collective.portletpage.toprow', view=self)
@@ -51,6 +76,14 @@ class FourColumns(Base):
     def hasColumnMiddleSecond(self):
         ploneview = self.plone_view()
         return ploneview.have_portlets('collective.portletpage.middlesecond', view=self)
+
+    def middleColumnCount(self):
+        i = 0
+        if self.hasColumnMiddleFirst():
+            i += 1
+        if self.hasColumnMiddleSecond():
+            i += 1
+        return i
 
     def hasColumnBottom(self):
         ploneview = self.plone_view()
